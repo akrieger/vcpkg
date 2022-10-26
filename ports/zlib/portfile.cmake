e1c48758d3472efe687b9a5816a5646fa53c82a6
Andrew Krieger <akrieger@users.noreply.github.com>
2022-10-26 12:18:02 -0700

    revert to zlib 1.2.11

--- ports/zlib/portfile.cmake
+++ ports/zlib/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO madler/zlib
-    REF v1.2.12
-    SHA512 5b029532a9f5f12ad425c12eccdf1b77c8d91801342c5b5e26ffb539f76a204e6c4882b40f0130f143f2cd38df90e90af2978cf4bb997e1fa3a0d1eff2ca979e
+    REF v1.2.11
+    SHA512 104c62ed1228b5f1199bc037081861576900eb0697a226cafa62a35c4c890b5cb46622e399f9aad82ee5dfb475bae26ae75e2bd6da3d261361b1c8b996970faf
     HEAD_REF master
     PATCHES
         cmake_dont_build_more_than_needed.patch
